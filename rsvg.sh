623c7ca7ec4d7a30e9d80faafc0d353c22286b59
awef <idawef@gmail.com>
2011-04-23 08:15:21 +0900

    アイコンの用意

--- rsvg.sh
+++ rsvg.sh
@@ -1,1 +1,4 @@
+rsvg -w 128 -h 128 image/svg/read.crx.svg code/img/read.crx_128x128.png
+rsvg -w 48 -h 48 image/svg/read.crx.svg code/img/read.crx_48x48.png
+rsvg -w 16 -h 16 image/svg/read.crx.svg code/img/read.crx_16x16.png
 rsvg -w 16 -h 16 image/svg/close.svg code/img/close_16x16.png
