a148d9acd24d889fd71e35b7743bc0dd6907c73a
awef <idawef@gmail.com>
2011-07-01 16:42:11 +0900

    バージョンアップ時の処理を一括して行うversion_change.sh作成

---
+++ version_change.sh
@@ -0,0 +1,10 @@
+sed -r -e "s/\"0\.[0-9]+\"/\"0\.$1\"/" -i src/manifest.json
+sed -r -e "s/app_version = \"0\.[0-9]+\"/app_version = \"0.$1\"/" -i src/*.haml src/write/*.haml
+
+make clean
+make
+
+rm ./read.crx_2.zip
+zip -9 -r -X -T ./read.crx_2.zip ./debug -x ./debug/test/\*
+
+google-chrome chrome-extension://pjgcfbpjgcmblkffcjfmdgcgmdhdaohh/test/test.html
