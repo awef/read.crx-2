f30a5159224f885d550925b153e19f20d03c137e
awef <idawef@gmail.com>
2011-08-02 03:16:51 +0900

    板ブックマーク関連のテスト作成

---
+++ src/test/test_app.bookmark.js
@@ -0,0 +1,78 @@
+module("app.bookmark");
+
+test("ブックマークされていないURLを取得しようとした時は、nullを返す", 1, function(){
+  strictEqual(app.bookmark.get("http://__dummy.2ch.net/dummy/"), null);
+});
+
+asyncTest("板のブックマークを保存/取得/削除出来る", 6, function(){
+  var url = "http://__dummy.2ch.net/dummy/";
+  var title = "ダミー板";
+  var expect_bookmark = {
+    type: "board",
+    bbs_type: "2ch",
+    title: title,
+    url: url,
+    res_count: null,
+    read_state: null,
+    expired: false
+  };
+
+  var deferred_on_added = $.Deferred();
+  var on_added = function(message){
+    deepEqual(message, {type: "added", bookmark: expect_bookmark});
+    deferred_on_added.resolve();
+    app.message.remove_listener("bookmark_updated", on_added);
+  };
+
+  var deferred_on_removed = $.Deferred();
+  var on_removed = function(message){
+    deepEqual(message, {type: "removed", bookmark: expect_bookmark});
+    deferred_on_removed.resolve();
+    app.message.remove_listener("bookmark_updated", on_removed);
+  };
+
+  //追加
+  app.message.add_listener("bookmark_updated", on_added);
+  $.when(app.bookmark.add(url, title), deferred_on_added)
+    .pipe(function(){
+      var deferred = $.Deferred();
+      //取得確認
+      deepEqual(app.bookmark.get(url), expect_bookmark);
+      chrome.bookmarks.getChildren(app.config.get("bookmark_id"), function(array_of_tree){
+        if(array_of_tree.some(function(tree){ return tree.url === url; })){
+          ok(true);
+          deferred.resolve();
+        }
+        else{
+          ok(false);
+          deferred.reject();
+        }
+      });
+      return deferred;
+    })
+    .pipe(function(){
+      //削除
+      app.message.add_listener("bookmark_updated", on_removed);
+      return $.when(app.bookmark.remove(url), deferred_on_removed);
+    })
+    .pipe(function(){
+      var deferred = $.Deferred();
+      //削除確認
+      strictEqual(app.bookmark.get(url), null);
+      chrome.bookmarks.getChildren(app.config.get("bookmark_id"), function(array_of_tree){
+        if(array_of_tree.some(function(tree){ return tree.url === url; })){
+          ok(false);
+          deferred.resolve();
+        }
+        else{
+          ok(true);
+          deferred.reject();
+        }
+      });
+      return deferred;
+    })
+    .always(function(){
+      start();
+    });
+});
+
