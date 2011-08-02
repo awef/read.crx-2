6bf2cc3474fdca5637a08729c768a784c0921d8e
awef <idawef@gmail.com>
2011-08-02 17:35:53 +0900

    app.bookmark.url_to_bookmarkのテスト作成

--- src/test/test_app.bookmark.js
+++ src/test/test_app.bookmark.js
@@ -1,3 +1,253 @@
+module("app.bookmark.url_to_bookmark");
+
+test("URLからブックマークオブジェクトを作成する", 16, function(){
+  var fixed_url = "http://__dummy.2ch.net/dummy/";
+  deepEqual(
+    app.bookmark.url_to_bookmark(fixed_url),
+    {
+      type: "board",
+      bbs_type: "2ch",
+      url: fixed_url,
+      title: fixed_url,
+      res_count: null,
+      read_state: null,
+      expired: false
+    },
+    "板URL"
+  );
+
+  fixed_url = "http://__dummy_server.2ch.net/test/read.cgi/__dummy_board/1234567890/";
+  deepEqual(
+    app.bookmark.url_to_bookmark(fixed_url),
+    {
+      type: "thread",
+      bbs_type: "2ch",
+      url: fixed_url,
+      title: fixed_url,
+      res_count: null,
+      read_state: null,
+      expired: false
+    },
+    "スレURL"
+  );
+
+  deepEqual(
+    app.bookmark.url_to_bookmark(fixed_url + "#res_count=123"),
+    {
+      type: "thread",
+      bbs_type: "2ch",
+      url: fixed_url,
+      title: fixed_url,
+      res_count: 123,
+      read_state: null,
+      expired: false
+    },
+    "スレURL + res_count"
+  );
+
+  deepEqual(
+    app.bookmark.url_to_bookmark(fixed_url + "#res_count"),
+    {
+      type: "thread",
+      bbs_type: "2ch",
+      url: fixed_url,
+      title: fixed_url,
+      res_count: null,
+      read_state: null,
+      expired: false
+    },
+    "スレURL + 不正なres_count(Boolean)"
+  );
+
+  deepEqual(
+    app.bookmark.url_to_bookmark(fixed_url + "#res_count=dummy"),
+    {
+      type: "thread",
+      bbs_type: "2ch",
+      url: fixed_url,
+      title: fixed_url,
+      res_count: null,
+      read_state: null,
+      expired: false
+    },
+    "スレURL + 不正なres_count(文字列)"
+  );
+
+  deepEqual(
+    app.bookmark.url_to_bookmark(fixed_url + "#expired"),
+    {
+      type: "thread",
+      bbs_type: "2ch",
+      url: fixed_url,
+      title: fixed_url,
+      res_count: null,
+      read_state: null,
+      expired: true
+    },
+    "スレURL + expired"
+  );
+
+  deepEqual(
+    app.bookmark.url_to_bookmark(fixed_url + "#expired=true"),
+    {
+      type: "thread",
+      bbs_type: "2ch",
+      url: fixed_url,
+      title: fixed_url,
+      res_count: null,
+      read_state: null,
+      expired: false //一見trueで良さそうだけれど、URLパラメータで指定されているのはあくまで"true"という文字列
+    },
+    "スレURL + expired(true)"
+  );
+
+  deepEqual(
+    app.bookmark.url_to_bookmark(fixed_url + "#expired=false"),
+    {
+      type: "thread",
+      bbs_type: "2ch",
+      url: fixed_url,
+      title: fixed_url,
+      res_count: null,
+      read_state: null,
+      expired: false
+    },
+    "スレURL + expired(false)"
+  );
+
+  deepEqual(
+    app.bookmark.url_to_bookmark(fixed_url + "#expired=123"),
+    {
+      type: "thread",
+      bbs_type: "2ch",
+      url: fixed_url,
+      title: fixed_url,
+      res_count: null,
+      read_state: null,
+      expired: false
+    },
+    "スレURL + 不正なexpired(数値文字列)"
+  );
+
+  deepEqual(
+    app.bookmark.url_to_bookmark(fixed_url + "#last=123&read=234&received=345"),
+    {
+      type: "thread",
+      bbs_type: "2ch",
+      url: fixed_url,
+      title: fixed_url,
+      res_count: null,
+      read_state: {
+        url: fixed_url,
+        last: 123,
+        read: 234,
+        received: 345
+      },
+      expired: false
+    },
+    "スレURL + read_state"
+  );
+
+  deepEqual(
+    app.bookmark.url_to_bookmark(fixed_url + "#last=test&read=234&received=345"),
+    {
+      type: "thread",
+      bbs_type: "2ch",
+      url: fixed_url,
+      title: fixed_url,
+      res_count: null,
+      read_state: null,
+      expired: false
+    },
+    "スレURL + 不正なread_state"
+  );
+
+  deepEqual(
+    app.bookmark.url_to_bookmark(fixed_url + "#read=234&received=345"),
+    {
+      type: "thread",
+      bbs_type: "2ch",
+      url: fixed_url,
+      title: fixed_url,
+      res_count: null,
+      read_state: null,
+      expired: false
+    },
+    "スレURL + 不完全なread_state"
+  );
+
+  deepEqual(
+    app.bookmark.url_to_bookmark(fixed_url + "#last=123&read=234&received=345&res_count=456"),
+    {
+      type: "thread",
+      bbs_type: "2ch",
+      url: fixed_url,
+      title: fixed_url,
+      res_count: 456,
+      read_state: {
+        url: fixed_url,
+        last: 123,
+        read: 234,
+        received: 345
+      },
+      expired: false
+    },
+    "スレURL + read_state + res_count"
+  );
+
+  deepEqual(
+    app.bookmark.url_to_bookmark(fixed_url + "#last=123&read=234&received=345&res_count=456&expired"),
+    {
+      type: "thread",
+      bbs_type: "2ch",
+      url: fixed_url,
+      title: fixed_url,
+      res_count: 456,
+      read_state: {
+        url: fixed_url,
+        last: 123,
+        read: 234,
+        received: 345
+      },
+      expired: true
+    },
+    "スレURL + read_state + res_count + expired"
+  );
+
+  deepEqual(
+    app.bookmark.url_to_bookmark(fixed_url + "123/?test=123#123"),
+    {
+      type: "thread",
+      bbs_type: "2ch",
+      url: fixed_url,
+      title: fixed_url,
+      res_count: null,
+      read_state: null,
+      expired: false
+    },
+    "スレURL + 無関係なオプション"
+  );
+
+  deepEqual(
+    app.bookmark.url_to_bookmark(fixed_url + "123/?test=123#last=123&read=234&received=345&res_count=456&expired"),
+    {
+      type: "thread",
+      bbs_type: "2ch",
+      url: fixed_url,
+      title: fixed_url,
+      res_count: 456,
+      read_state: {
+        url: fixed_url,
+        last: 123,
+        read: 234,
+        received: 345
+      },
+      expired: true
+    },
+    "スレURL + read_state + res_count + expired + 無関係なオプション"
+  );
+});
+
 module("app.bookmark", {
   setup: function(){
     this.one = function(type, listener){
