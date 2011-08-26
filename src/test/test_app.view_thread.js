41ce71ae2e4e5a7e0bf217608e953beb565b3d89
awef <idawef@gmail.com>
2011-08-26 17:11:36 +0900

    テストのリファクタリング

--- src/test/test_app.view_thread.js
+++ src/test/test_app.view_thread.js
@@ -4,51 +4,47 @@ $(function(){
       this.$view = $("<div>");
       this.$view.data("id_index", {});
       this.$view.data("rep_index", {});
+
+      this.example1_data = {
+        name: "名無しさん",
+        mail: "sage",
+        other: "2010/05/14(木) 15:41:14 ID:iTGL5FKU",
+        message: "test"
+      };
+      this.example1_dom = $("<article>")
+        .attr("data-id", "ID:iTGL5FKU")
+        .append(
+          $("<header>")
+            .append(
+              $("<span class=\"num\">").text("1"),
+              $("<span class=\"name\">").text("名無しさん"),
+              $("<span class=\"mail\">").text("sage"),
+              $("<span class=\"other\">")
+                .append(document.createTextNode("2010/05/14(木) 15:41:14 "))
+                .append($("<span class=\"id\">").text("ID:iTGL5FKU"))
+            )
+        )
+        .append(
+          $("<div class=\"message\">").text("test")
+        )[0];
     }
   });
 
   test("レスのデータからDOMを生成し、id_index/rep_indexを更新する", 6, function(){
-    var expected1 = '\
-<article data-id="ID:iTGL5FKU">\
-<header>\
-<span class="num">1</span>\
-<span class="name">名無しさん</span>\
-<span class="mail"></span>\
-<span class="other">2010/05/14(木) 15:41:14 <span class="id">ID:iTGL5FKU</span></span>\
-</header>\
-<div class="message">test</div>\
-</article>\
-';
-     var expected2 = '\
-<article data-id="ID:iTGL5FKU">\
-<header>\
-<span class="num">2</span>\
-<span class="name">名無しさん</span>\
-<span class="mail"></span>\
-<span class="other">2010/05/14(木) 15:41:14 <span class="id">ID:iTGL5FKU</span></span>\
-</header>\
-<div class="message">test<br><a href="javascript:undefined;" class="anchor">&gt;&gt;1</a></div>\
-</article>\
-';
-    var $container1 = $("<div>");
-    $container1.append(app.view_thread._const_res(0, {
-      name: "名無しさん",
-      mail: "",
-      other: "2010/05/14(木) 15:41:14 ID:iTGL5FKU",
-      message: "test"
-    }, this.$view));
-    strictEqual($container1.html(), expected1);
+    var tmp_dom;
+
+    //>>1
+    tmp_dom = app.view_thread._const_res(0, this.example1_data, this.$view);
+    strictEqual(tmp_dom.outerHTML, this.example1_dom.outerHTML);
     deepEqual(this.$view.data("id_index"), {"ID:iTGL5FKU": [0]});
     deepEqual(this.$view.data("rep_index"), {});
 
-    var $container2 = $("<div>");
-    $container2.append(app.view_thread._const_res(1, {
-      name: "名無しさん",
-      mail: "",
-      other: "2010/05/14(木) 15:41:14 ID:iTGL5FKU",
-      message: "test<br>&gt;&gt;1"
-    }, this.$view));
-    strictEqual($container2.html(), expected2);
+    //>>2
+    this.example1_data.message = "test<br>&gt;&gt;1";
+    this.example1_dom.querySelector(".num").textContent = "2";
+    this.example1_dom.querySelector(".message").innerHTML = 'test<br><a href="javascript:undefined;" class="anchor">&gt;&gt;1</a>';
+    tmp_dom = app.view_thread._const_res(1, this.example1_data, this.$view);
+    strictEqual(tmp_dom.outerHTML, this.example1_dom.outerHTML);
     deepEqual(this.$view.data("id_index"), {"ID:iTGL5FKU": [0, 1]});
     deepEqual(this.$view.data("rep_index"), {1: [1]});
   });
@@ -56,25 +52,18 @@ $(function(){
   test("もし元データにscriptタグ等が入っていても、無視する", 3, function(){
     //基本的にタグは除去
     //ただし名前欄はニダーのAAが入る事が有るのでエスケープに
-    var expected1 = '\
-<article data-id="ID:iTGL5FKU">\
-<header>\
-<span class="num">1</span>\
-<span class="name">&lt;script&gt;名無しさん&lt;/script&gt;</span>\
-<span class="mail">alert();sage</span>\
-<span class="other">2010/05/14(木) 15:41:14 <span class="id">ID:iTGL5FKU</span></span>\
-</header>\
-<div class="message">testalert();</div>\
-</article>\
-';
-    var $container1 = $("<div>");
-    $container1.append(app.view_thread._const_res(0, {
-      name: "<script>名無しさん</script>",
-      mail: "<script>alert();</script>sage",
-      other: "2010/05/14(木) 15:41:14 <script>ID:iTGL5FKU</script>",
-      message: "test<script>alert();</script>"
-    }, this.$view));
-    strictEqual($container1.html(), expected1);
+    var tmp_dom;
+
+    this.example1_data.name = "<script>名無しさん</script>";
+    this.example1_dom.querySelector(".name").innerHTML = "&lt;script&gt;名無しさん&lt;/script&gt;";
+    this.example1_data.mail = "<script>alert();</script>sage";
+    this.example1_dom.querySelector(".mail").innerHTML = "alert();sage";
+    this.example1_data.other = "2010/05/14(木) 15:41:14 <script>ID:iTGL5FKU</script>";
+    this.example1_dom.querySelector(".other").innerHTML = '2010/05/14(木) 15:41:14 <span class="id">ID:iTGL5FKU</span>';
+    this.example1_data.message = "test<script>alert();</script>";
+    this.example1_dom.querySelector(".message").innerHTML = "testalert();";
+    tmp_dom = app.view_thread._const_res(0, this.example1_data, this.$view);
+    strictEqual(tmp_dom.outerHTML, this.example1_dom.outerHTML);
     deepEqual(this.$view.data("id_index"), {"ID:iTGL5FKU": [0]});
     deepEqual(this.$view.data("rep_index"), {});
   });
